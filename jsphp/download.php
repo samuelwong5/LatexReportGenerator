52a066fe195022966002a7b33267c7186520d767
samuelwong5 <swong55@gmail.com>
2015-07-17 15:17:36 +0800

    Deleted testing methods, changed save filepaths for images to /home/samuelwong/Report/graphs (same as report.py python script

--- jsphp/download.php
+++ jsphp/download.php
@@ -1,8 +1,11 @@
 <?php
 
-$path = "graphs/";
+// Path to save images to
+$path = "/home/samuelwong/Report/graphs/";    
 $img = urldecode($_POST["s"]);
 $name = $_POST["name"];
+
+// Strip headers
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
@@ -10,6 +13,5 @@ $fpath = $path . $name . ".png";
 $file = fopen($fpath, "wb") or die(print_r(error_get_last(),true));
 $bytes = $bytes . fwrite($file, $data);
 fclose($file);
-echo $fpath;
 
 ?>
--- jsphp/readcsv_beta.php
+++ jsphp/readcsv_beta.php
@@ -1,6 +1,7 @@
 <?php
 $f_path = $_REQUEST["file"];
-$file = fopen($f_path,"r");
+$data_path = "/home/samuelwong/Report/HKSWROutput/report/";
+$file = fopen($data_path . $f_path,"r");
 if(!$file) die("[ERROR] File not found.");
 $data = array();
 $header = fgetcsv($file);
@@ -10,6 +11,5 @@ while(!feof($file))
   $temp = fgetcsv($file);
   array_push($data, array($temp[0], (int) $temp[1]));
 }
-//echo "hello world";
 echo json_encode($data);
 ?>
